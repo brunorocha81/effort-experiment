3066b9ddd8c4cff994864b1d386a608eb79ab6c2
Breno Felix <bfs@ic.ufal.br>
2022-05-12 23:23:33 -0300

    Update server.js

--- server.js
+++ server.js
@@ -46,5 +46,5 @@ app.post('/save-response', function(req, res, next){
 
 });
 
-app.listen(8063, https://weblab.nees.com.br/bruno/);
+app.listen(8063, 'localhost');
 console.log("This project is listening on port 8063");
